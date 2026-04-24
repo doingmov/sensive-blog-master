ea9685841b4e8e8ec30da9c6ac1d459c17886506
doingmov <yohei13111@mail.ru>
2026-04-24 19:35:03 +0300

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,38 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
+from django.db.models import Count, Prefetch
+
+
+class PostQuerySet(models.QuerySet):
+
+    def popular(self):
+        return self.annotate(likes_count=Count('likes')).order_by('-likes_count')
+
+    def fetch_with_comments_count(self):
+        posts = self.all()
+        posts_ids = [post.id for post in posts]
+
+        posts_with_comments = Post.objects.filter(id__in=posts_ids).annotate(comments_count=Count('comments'))
+        ids_and_comments = dict(posts_with_comments.values_list('id', 'comments_count'))
+
+        for post in posts:
+            post.comments_count = ids_and_comments[post.id]
+
+        return posts
+
+
+class TagQuerySet(models.QuerySet):
+
+    def popular(self):
+        return self.annotate(posts_count=Count('posts')).order_by('-posts_count')
+
+    def with_posts_count(self):
+        return self.annotate(posts_with_tag=Count('posts'))
+
+    def prefetch_with_posts_count(self):
+        tag_queryset = self.with_posts_count()
+        return Prefetch('tags', queryset=tag_queryset)
 
 
 class Post(models.Model):
@@ -14,16 +46,26 @@ class Post(models.Model):
         User,
         on_delete=models.CASCADE,
         verbose_name='Автор',
-        limit_choices_to={'is_staff': True})
+        limit_choices_to={'is_staff': True}
+    )
     likes = models.ManyToManyField(
         User,
         related_name='liked_posts',
         verbose_name='Кто лайкнул',
-        blank=True)
+        blank=True
+    )
     tags = models.ManyToManyField(
         'Tag',
         related_name='posts',
-        verbose_name='Теги')
+        verbose_name='Теги'
+    )
+
+    objects = PostQuerySet.as_manager()
+
+    class Meta:
+        ordering = ['-published_at']
+        verbose_name = 'пост'
+        verbose_name_plural = 'посты'
 
     def __str__(self):
         return self.title
@@ -31,14 +73,10 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse('post_detail', args={'slug': self.slug})
 
-    class Meta:
-        ordering = ['-published_at']
-        verbose_name = 'пост'
-        verbose_name_plural = 'посты'
-
 
 class Tag(models.Model):
     title = models.CharField('Тег', max_length=20, unique=True)
+    objects = TagQuerySet.as_manager()
 
     def __str__(self):
         return self.title
@@ -59,7 +97,9 @@ class Comment(models.Model):
     post = models.ForeignKey(
         'Post',
         on_delete=models.CASCADE,
-        verbose_name='Пост, к которому написан')
+        verbose_name='Пост, к которому написан',
+        related_name='comments'
+    )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
